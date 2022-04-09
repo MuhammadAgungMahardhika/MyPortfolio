c1c3cce3d1e0d32acd33b6b9f412cf269725d99f
Muhammad Agung Mahardhika <m.agungmahardika12@gmail.com>
2022-04-09 15:50:24 +0700

    ok

--- function.js
+++ function.js
@@ -1,23 +1,23 @@
 // smooth scrooling
 
-jQuery(document).ready(function($) {
-    var scrollSpy = new bootstrap.ScrollSpy(document.body, {target: '#navbar-example'});
+jQuery(document).ready(function ($) {
+    var scrollSpy = new bootstrap.ScrollSpy(document.body, { target: '#navbar-example' });
 
-        $('#MyName .NamaSaya').addClass('Muncul');
-        $('#MyFoto .rounded-circle').addClass('muncul');
-       
-    $('#MyCertificates .card').hover(function(){
-            $('.star').css({
-                'transition' : '2s',
-                'color': 'goldenrod'
-            });
+    $('#MyName .NamaSaya').addClass('Muncul');
+    $('#MyFoto .rounded-circle').addClass('muncul');
+
+    $('#MyCertificates .card').hover(function () {
+        $('.star').css({
+            'transition': '2s',
+            'color': 'gold'
+        });
     });
 
     $(window).scroll(function () {
 
         var wScrool = $(this).scrollTop();
         // parallax efex pada tulisan Nama saya
-        
+
 
         $('#MyFoto').css({
             'transform': 'translate(0px,' + wScrool / 12 + '%)'
@@ -34,7 +34,7 @@ jQuery(document).ready(function($) {
         //   $('#Nav').css({
         //     'position' : 'fixed',
         //     'background-color' : 'rgb(52,56,114)'
-            
+
         //   });
         // }else if(wScrool < $('#AboutMe').offset().top-0){
         //   $('#Nav').css({
@@ -45,20 +45,20 @@ jQuery(document).ready(function($) {
 
         //parralax efek pada my projects
 
-        if(wScrool > $('#MyProjects').offset().top-200){
+        if (wScrool > $('#MyProjects').offset().top - 200) {
             $('.card ').addClass('muncul');
-         
-        }else if(wScrool > $('#AboutMe').offset().top-200){
+
+        } else if (wScrool > $('#AboutMe').offset().top - 200) {
             $('.efek1').addClass('Muncul');
-            
-        }else if(wScrool > $('#MyProjects').offset().top-200){
+
+        } else if (wScrool > $('#MyProjects').offset().top - 200) {
             $('.efek1').addClass('Muncul');
-            
+
         }
 
 
     });
 
-  
+
 
 });
